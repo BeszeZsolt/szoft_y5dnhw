fa36003da570b904f7d787169c37fee4c73dc13e
BeszeZsolt <zsolt.besze@stud.uni-corvinus.hu>
2024-05-02 13:11:54 +0200

    harmadik

--- 0502/10gyak/Form1.cs
+++ 0502/10gyak/Form1.cs
@@ -15,11 +15,8 @@ namespace _10gyak
 
             g.Clear(Color.Black);
 
-            Color c = Color.White;
-
-            Pen toll = new Pen(c, 1);
-            Brush brush = new SolidBrush(c);
-            //g.DrawLine(toll, 10, 10, 10, 10);
+            Pen pen = new Pen(Color.AntiqueWhite, 1);
+            Brush brush = new SolidBrush(Color.White);
 
 
             hajosContext context = new hajosContext();
@@ -38,6 +35,26 @@ namespace _10gyak
                 double size = 20*Math.Pow(10, s.Magnitude / -2.5);
 
                 g.FillEllipse(brush, x, y, (float)size, (float)size);
+            }
+
+            var lines = context.ConstellationLines.ToList();
+
+            foreach (var l in lines)
+            {
+
+                var star1 = (from s in stars where s.Hip == l.Star1 select s).FirstOrDefault();
+                var star2 = (from s in stars where s.Hip == l.Star2 select s).FirstOrDefault();
+
+                if (star1==null || star2==null) continue;
+                if ((Math.Sqrt(Math.Pow(star1.X, 2) + Math.Pow(star1.Y, 2)) > 1) &
+                    (Math.Sqrt(Math.Pow(star2.X, 2) + Math.Pow(star2.Y, 2)) > 1)) continue;
+                float star1x = ((float)star1.X * nagyítás + cx);
+                float star1y = ((float)star1.Y * nagyítás + cy);
+                float star2x = ((float)star2.X * nagyítás + cx);
+                float star2y = ((float)star2.Y * nagyítás + cy);
+
+
+                g.DrawLine(pen, star1x, star1y, star2x, star2y);
             }
         }
     }
